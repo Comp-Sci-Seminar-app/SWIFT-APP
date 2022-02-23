bd480e5ffcaeee1c9513d6910fd2d52b5f5f7348
Ari Steinfeld <crazybonesx@gmail.com>
2022-02-23 08:57:35 -0500

    added the actual xcode and pod stuff to the project

---
+++ ForeCaster/ForeCaster/ForeCasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeCasterApp.swift
+//  ForeCaster
+//
+//  Created by Ari Steinfeld (student LM) on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ForeCasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForeCaster/Pods/Target Support Files/Pods-ForeCaster/Pods-ForeCaster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForeCaster : NSObject
+@end
+@implementation PodsDummy_Pods_ForeCaster
+@end
