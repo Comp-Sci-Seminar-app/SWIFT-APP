02286d34ebfd10d1cf9483acee76de6e3e3e3046
Ari Steinfeld <crazybonesx@gmail.com>
2022-03-01 09:28:40 -0500

    api is working so we can actually do stuff

--- ForeCaster/ForeCaster/AdvancedHourlyView.swift
+++ ForeCaster/ForeCaster/AdvancedHourlyView.swift
@@ -12,12 +12,35 @@ struct AdvancedHourlyView: View {
     
     var body: some View {
         ZStack{
+            ZStack{
             VStack{
-                Text("sadness")
-            }.frame(width: UIScreen.main.bounds.width - 20, height: UIScreen.main.bounds.height - 20)
-            .background(Color.gray)
-            
-        }.background(Color.blue)
+                Spacer()
+                Text("Forcast for \(makeTimeNice(data.startTime))").frame(width: UIScreen.main.bounds.width - 80, height: 50).background(Color.gray.opacity(0.8)).cornerRadius(10)
+                Spacer()
+                VStack{
+                    HStack{
+                        Spacer()
+                        Text("Wind speed: \(data.windSpeed)")
+                        Spacer()
+                        Text("Wind direction \(data.windDirection)")
+                        Spacer()
+                    }.frame(width: UIScreen.main.bounds.width - 80, height: 40)
+                    HStack{
+                        Spacer()
+                        Text("Temperature: \(data.temperature)")
+                        Spacer()
+                        Text("number? \(data.number)")
+                        Spacer()
+                    }.frame(width: UIScreen.main.bounds.width - 80, height: 40)
+                    HStack{
+                        Text("Short Forcast: \(data.shortForecast ?? "no data bro")")
+                    }.frame(width: UIScreen.main.bounds.width - 80, height: 40)
+                }.frame(width: UIScreen.main.bounds.width - 80, height: 120)
+                Spacer()
+            }.frame(width: UIScreen.main.bounds.width - 60, height: UIScreen.main.bounds.height - 60).background(Color.gray.opacity(0.8))
+            .background(Color.blue).cornerRadius(30)
+            }.frame(width: UIScreen.main.bounds.width-30, height: UIScreen.main.bounds.height-30).background(Color.gray).cornerRadius(30)
+        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height).background(Color.blue).ignoresSafeArea(.all)
     }
 }
 
--- ForeCaster/ForeCaster/ContentView.swift
+++ ForeCaster/ForeCaster/ContentView.swift
@@ -6,38 +6,90 @@
 //
 
 import SwiftUI
+func makeTimeNice(_ evilTime: String)-> String{
+    @AppStorage("24h") var twentyFourHourClock = true
+    //2022-02-24T(11)10:00:00-05:00
+
+    
+
+    let start = evilTime.dropFirst(11)
+
+    let end = start.dropLast(9)
+
+    var niceTime = String(end)
+
+    if niceTime.first == "0"{
+
+        niceTime = String(niceTime.dropFirst())
+
+    }
+
+    else if !twentyFourHourClock{
+
+        for i in 13...24{
+
+            if niceTime.hasPrefix(String(i)){
+
+                if !twentyFourHourClock{
+
+                    return String(i-12)+String(niceTime.dropFirst(2))+" PM"
+
+                }
+
+                else{
+
+                    return String(i-12)+String(niceTime.dropFirst(2))
+
+                }
+
+            }
+
+        }
+
+    }
+
+    if !twentyFourHourClock{
+
+        return niceTime+" AM"
+
+    }
+
+    else{
+
+        return niceTime
+
+    }
+
+}
 
 struct ContentView: View {
     @StateObject var g = Decoded()
     @StateObject var f = FetchData()
+    @AppStorage("24h") var hours : Bool = true
     var body: some View {
-        // NavigationView{
-        //     List{
-        //        ForEach(0..<156) { index in
+         NavigationView{
+             List{
+                Button(action: {hours.toggle()}) {
+                    Text("change hours")
+                }
+                ForEach(0..<156) { index in
         
-        //            NavigationLink(
-        //                destination:
-        //                    AdvancedHourlyView(data: $g.hForecast.properties.periods[index])
+                    NavigationLink(
+                        destination:
+                            AdvancedHourlyView(data: $g.hForecast.properties.periods[index])
         
-        //               , label: {
-        
-        
-        //                   Text("\(index)")
-        
-        //                })
+                       , label: {
         
         
-        //         }
-        //     }
-        ZStack{
-            VStack{
-                Text("sadness")
-            }.frame(width: UIScreen.main.bounds.width - 40, height: UIScreen.main.bounds.height - 40, alignment: .center).background(Color.gray).cornerRadius(20)
-            
-            
-        }.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .center).background(Color.blue).ignoresSafeArea(.all)
-        Spacer()
+                           Text("\(index)")
         
+                        })
+        
+        
+                 }
+             }
+        
+         }
     }
 }
 
