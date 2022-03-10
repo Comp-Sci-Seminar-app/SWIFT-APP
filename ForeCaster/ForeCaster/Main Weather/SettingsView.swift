163b8577afd0ad58b6dd30fc5eb724d7d2036444
Ari Bredbenner <s015596@students.lmsd.org>
2022-03-10 08:07:40 -0500

    Now you can make any time based notification with 1 click. Needs to be set up in the code first.

--- ForeCaster/ForeCaster/Main Weather/SettingsView.swift
+++ ForeCaster/ForeCaster/Main Weather/SettingsView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State var reminderEnabled: Bool = false
+    @State var reminderEnabled: Bool = true
     @State var selectedTrigger = ReminderType.time
     @State var timeDurationIndex: Int = 0
     @State private var shouldRepeat = true
     @Environment(\.presentationMode) var presentationMode
     @StateObject var locationManager = LocationManager()
     
-    let timeDurations: [Int] = Array(1...59)
-    func makeReminder() -> Reminder? {
+    let timeDurations: [Int] = Array(1...60)
+    func makeReminder(time: Int) -> Reminder? {
         guard reminderEnabled else {
             return nil
         }
+        timeDurationIndex = time
         var reminder = Reminder()
         reminder.reminderType = selectedTrigger
         reminder.timeInterval = TimeInterval(timeDurations[timeDurationIndex] * 60)
@@ -28,11 +29,21 @@ struct SettingsView: View {
     }
     
     var body: some View{
-        
-        Button("Create 1 minute notification") {
-            TaskManager.shared.addNewTask("1 minute notification", makeReminder())
-            presentationMode.wrappedValue.dismiss()
+        VStack{
+            Toggle(isOn: $reminderEnabled) {
+                Text("Add Reminder")
+            }
+            .padding(.vertical)
+ 
+            Button("Create 1 minute notification") {
+                TaskManager.shared.addNewTask("1 minute notification", makeReminder(time: 0))
+                presentationMode.wrappedValue.dismiss()
+            }
+            
+            Button("Create 2 minute notification") {
+                TaskManager.shared.addNewTask("2 minute notification", makeReminder(time: 1))
+                presentationMode.wrappedValue.dismiss()
+            }
         }
     }
-    
 }
