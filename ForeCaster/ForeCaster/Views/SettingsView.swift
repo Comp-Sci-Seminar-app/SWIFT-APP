7e5dad5bcdc3d3fa0fec1154492efdcacce2d86a
Ari Bredbenner <s015596@students.lmsd.org>
2022-04-18 09:12:26 -0400

    Added some comments

--- ForeCaster/ForeCaster/Views/SettingsView.swift
+++ ForeCaster/ForeCaster/Views/SettingsView.swift
@@ -16,6 +16,7 @@ struct SettingsView: View {
         let allHourly = g.hForecast.properties.periods
 
         VStack {
+            //Requests permission to send notifications
             Button("Request Permission") {
                 UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
                     if success {
@@ -26,22 +27,32 @@ struct SettingsView: View {
                 }
             }
             
+            //Makes notifications based on specified word
             Button("Schedule Notification") {
                 UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
                 UNUserNotificationCenter.current().removeAllDeliveredNotifications()
                 
+                //Creates the notifications
                 for i in returnHourList(hp: allHourly){
                     let content = UNMutableNotificationContent()
+                    //Title
                     content.title = "Weather Notification"
+                    //Subtitle or message
                     content.subtitle = "Rain"
+                    //Custom sound
                     content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "kricketune.mp3"))
+                    //When it triggers
                     let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(i * 2), repeats: false)
+                    //Create request
                     let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    //add the request fully
                     UNUserNotificationCenter.current().add(request)
+                    //Debug
                     print("\(i)")
                 }
                 
             }
+            //Removes all notifications that exist
             Button("Remove Notifications") {
                 UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
                 UNUserNotificationCenter.current().removeAllDeliveredNotifications()
